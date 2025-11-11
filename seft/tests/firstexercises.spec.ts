80c614ff57b9a13d20dd80af03f0f7438edafc70
NhungHuynh <nhunghuynh@kms-technology.com>
2025-11-12 01:55:37 +0700

    Add code to use Test Hooks

--- seft/tests/firstexercises.spec.ts
+++ seft/tests/firstexercises.spec.ts
@@ -1,12 +1,20 @@
 import {test, expect} from '@playwright/test';
 
-test('TC01: Verify page title is Playwright', async ({page}) => {
-    await page.goto('https://playwright.dev/');
-    await expect(page).toHaveTitle(/Playwright/);
-});
+//Declare a group of test
+test.describe('First Exercise - Playwright Demo', () => {
+    test.beforeEach(async ({page}) => {
+        //Open Home URL
+        await page.goto('https://playwright.dev/');
+    });
 
-test('TC02: Verify Get Started button navigates to Installation page', async({page}) => {
-    await page.goto('https://playwright.dev/');
-    await page.click('text=Get Started');
-    await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
+    test('TC01: Verify page title is Playwright', async ({page}) => {
+        // await page.goto('https://playwright.dev/');
+        await expect(page).toHaveTitle(/Playwright/);
+    });
+
+    test('TC02: Verify Get Started button navigates to Installation page', async({page}) => {
+        // await page.goto('https://playwright.dev/');
+        await page.getByRole('link', {name: 'Get started'}).click(); 
+        await expect(page.getByRole('heading', {name: 'Installation'})).toBeVisible();
+    });
 });
